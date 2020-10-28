6d7a3b85c247fdfd92ccfe4e4e21440cde081aa5
silwalanish <silwalanish@gmail.com>
2020-10-28 10:09:43 +0545

    (chore): Setup project structure

---
+++ README.md
@@ -0,0 +1,14 @@
+# ThreeDify-SfM
+A wrapper SfM pipeline built on top of [OpenSfM](https://github.com/mapillary/OpenSfM)
+
+# Setup
+1. Install `python-3.8` and `pip`.
+2. Install requirements.
+```bash
+$ pip install -r requirements.txt
+```
+3. Install OpenSfM in your environment. [Guide](https://www.opensfm.org/docs/building.html)
+
+If you are using archlinux, then read the following gist.
+[Setup OpenSfM on Archlinux](https://gist.github.com/silwalanish/ea80e66cd0a9898d370c08e0d9c84f05)
+
---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+requirements = []
+
+dev_requirements = [
+    "black==20.8b1",
+    "pylint==2.6.0"
+]
+
+setup(
+    name="threedify-sfm",
+    version="0.0.1",
+    author="Anish Silwal Khatri",
+    author_email="silwalanish@gmail.com",
+    description="A small SfM pipeline",
+    license="MIT",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Threedify/Threedify-SfM",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    include_package_data=True,
+    package_data={"": ["data/*.*"]},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=requirements,
+    extras_require={"dev": dev_requirements},
+)
